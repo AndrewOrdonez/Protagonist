c4a67a53031429d6d41376854d25a9353b8a7d07
Mike McGuire <MoonshotMike@gmail.com>
2016-10-17 10:56:39 -0600

    Initial commit

---
+++ Protagonist/src/Protagonist.java
@@ -0,0 +1,12 @@
+public class Protagonist
+	{
+	protected int numberOfVillainsThwarted = 20;
+	public Protagonist()
+		{System.out.println("We win in the end");}
+	public Protagonist(String name)
+		{System.out.println("It is " + name + " who has saved you");}
+	public void utterTagline()
+		{System.out.println("Go, good guys");}
+	public void rideOffIntoTheSunset()
+		{System.out.println("Adios, Amigos");}
+	}
---
+++ Protagonist/src/Superhero.java
@@ -0,0 +1,18 @@
+public class Superhero extends Protagonist	{
+	protected int numberOfVillainsThwarted = 200;
+	public Superhero()
+		{System.out.println("My planet sends you greetings");}
+	public Superhero(String name)
+		{System.out.println("It is " + name + " who has saved you");}
+	public void utterTagline()
+		{
+		//super.utterTagline();
+		System.out.println("Shazam");}
+	public void useSuperpower()
+		{System.out.println("(Insert graphic novel sound here)");}
+ 	public void getNumberOfVillainsThwarted() 
+{
+System.out.println(super.numberOfVillainsThwarted); 
+System.out.println(numberOfVillainsThwarted); 
+}
+	}
